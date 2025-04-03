73a48026d6198a38e13263ac7f21937067075f1c
Artyom <144758176+SingoSkhron@users.noreply.github.com>
2025-04-03 11:40:26 +0300

    Add files via upload

---
+++ UniversitySchedule/Domain/Entities/Group.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Group
+    {
+        public string GroupName { get; set; }
+        public string HigherEducationLevel { get; set; }
+        public string FormOfEducation { get; set; }
+        public int YearOfAdmission { get; set; }
+    }
+}
